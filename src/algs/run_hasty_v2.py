63214754d2c454138c8b6067c0ceb228ae0ac438
1979914078@qq.com <C:/Users/19799>
2024-04-08 14:32:20 +0800

    合并前提交

--- src/algs/run_hasty_v2.py
+++ src/algs/run_hasty_v2.py
@@ -1,14 +1,14 @@
 import numpy as np
 from stable_baselines3.common.env_util import make_vec_env
-
 from envs.rand_energy_v2_0 import SysEnv
+import csv
 # 种子相同时，就算env_num变化，第0个生成的环境始终是一样的
 env_num = 10
 vec_env = make_vec_env(SysEnv, n_envs=env_num, seed=2)
 obs = vec_env.reset()
 
 dones = np.zeros(env_num,dtype=bool)
-cum_rewards=np.zeros(env_num)
+cum_data=np.zeros(env_num)
 
 def power_to_data(p, h_i):
     # todo 或者
@@ -20,22 +20,19 @@ def data_to_power(data, h_i):
     return np.float32(energy)
 
 while not dones.any():
-    print(f"obs:{obs}")
-    # action_arr = []
-    # for obs in obs:
-    #     rest_energy = obs[0]
-    #     rest_data = obs[3]
-    #     rest_gain = obs[2]
-    #     rest_data_to_energy = data_to_power(rest_data, rest_gain)
-    #     action = min(rest_energy, rest_data_to_energy)
-    #     action_arr.append([action])
-    #
-    # print(f"actions:{action_arr}")
+    # print(f"obs:{obs}")
     obs, rewards, dones, infos = vec_env.step(obs[:,0].reshape(-1,1))
-    print(f'rewards:{rewards}')
-    print(f'data:{[info["data"] for info in infos]}')
-    cum_rewards += [info["data"] for info in infos]
-print(cum_rewards)
-print(np.mean(cum_rewards))
+    # print(f'rewards:{rewards}')
+    # print(f'data:{[info["data"] for info in infos]}')
+    data = [info["data"] for info in infos]
+    cum_data += data
+    with open('csv/hasty_p2.csv', mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['num', 'data'])
+        for i, data in enumerate(cum_data):
+            writer.writerow((i, data))
+
+
+
 
 
